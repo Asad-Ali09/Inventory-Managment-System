4be382c412070d592e899495a1fc7be6ec70a75b
Asad Shafiq <masadali5433@gmail.com>
2023-07-28 01:49:40 +0500

    updated header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/logo-light.png";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, NavLink } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -8,25 +8,27 @@ const Header = () => {
   return (
     <>
       <header className="header">
-        <div className="header__logo-box" onClick={() => navigate("/")}>
-          <img src={logo} alt="Logo" className="header__logo" />
+        <div className="header__content">
+          <div className="header__logo-box" onClick={() => navigate("/")}>
+            <img src={logo} alt="Logo" className="header__logo" />
 
-          <div className="header__logo--text">Coin Wise</div>
+            <div className="header__logo--text">Coin Wise</div>
+          </div>
+
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="nav__list--item">
+                <Link to={"/"}>Home</Link>
+              </li>
+              <li className="nav__list--item">
+                <Link to={"/market"}>Market</Link>
+              </li>
+              <li className="nav__list--item">
+                <Link to={"/"}>Join</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-
-        <nav className="nav">
-          <ul className="nav__list">
-            <li className="nav__list--item">
-              <Link to={"/"}>Home</Link>
-            </li>
-            <li className="nav__list--item">
-              <Link to={"/market"}>Market</Link>
-            </li>
-            <li className="nav__list--item">
-              <Link to={"/"}>Join</Link>
-            </li>
-          </ul>
-        </nav>
       </header>
     </>
   );
